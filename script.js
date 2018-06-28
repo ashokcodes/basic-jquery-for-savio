f5e7242f64eef7e33938c1e3e7f4b836696e0d39
Ashok Mohanakumar <ashok.mohank@gmail.com>
2018-06-28 21:22:16 +0530

    Add input to ul dynamic

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 
     $("#theButton").click(function(){
-        items = ["Apple","Mango","Pineapple"];
-        dom = "";
-        for(i=0;i < items.length; i++){
-            dom+= "<li class='list-group-item'>"+ items[i] +"</li>";
-        }
-        $("#blah").append(dom);  
+        msg = $("#msg").val();
+        $("#blah").append("<li class='list-group-item'>"+msg+"</li>");
     });
 });
